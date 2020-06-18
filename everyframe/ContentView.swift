5ca9ac2d90fffecfd36e3d2d1ab2b52e5cfe6949
Mikhail Apurin <m_apurin@yumemi.co.jp>
2020-06-18 11:56:55 +0900

    Initial Commit

---
+++ everyframe/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  everyframe
+//
+//  Created by Mikhail Apurin on 2020/06/18.
+//  Copyright © 2020 Mikhail Apurin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
